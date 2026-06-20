41f068f354504fec90c55fcf8ee46ac224003e8c
kobe-ham <robmonster.dev@gmail.com>
2026-06-20 15:19:26 +0900

    initial commit

---
+++ src/main/java/com/bidly/BidlyApplication.java
@@ -0,0 +1,13 @@
+package com.bidly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidlyApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BidlyApplication.class, args);
+    }
+
+}
